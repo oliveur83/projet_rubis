746eabf15bd7f5e74d8eb1db5ee8040074d84894
oliveur83 <tomolivierpro@gmail.com>
2025-07-21 20:36:45 +0200

    mise en place des setup

--- src/app/algo-oll/algo-oll.component.ts
+++ src/app/algo-oll/algo-oll.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Rubis3D, ActionKey } from '../../rubis3D';
-import { algoOLL } from '../constant';
+import { algoOLL, mettre_algoOLL } from '../constant';
+import { ShapeUtils } from 'three';
 @Component({
   selector: 'app-algo-oll',
   standalone: true,
@@ -23,8 +24,71 @@ export class AlgoOLLComponent {
     containerElements.forEach((container: HTMLElement, index: number) => {
       const rubis3D = new Rubis3D(container);
       rubis3D.init();
+
       this.rubis3DInstances[index] = rubis3D;
+      this.setup(index);
     });
+  }
+  setup(index: number) {
+    const rubis3DInstance = this.rubis3DInstances[index];
+
+    if (rubis3DInstance) {
+      const sequence: ActionKey[][] = mettre_algoOLL.map((str) => {
+        const result: ActionKey[] = [];
+        const clean = str.replace(/\s+/g, '').split('');
+
+        for (let i = 0; i < clean.length; i++) {
+          const char = clean[i];
+          const next = clean[i + 1];
+          const prec = clean[i - 1];
+          if (next === "'") {
+            if (char === 'r') {
+              result.push('rww' as ActionKey);
+            } else if (char === 'f') {
+              result.push('fww' as ActionKey);
+            } else if (char === 'l') {
+              result.push('lww' as ActionKey);
+            } else {
+              result.push(char.toLowerCase() as ActionKey);
+            } // Prime move
+            i++; // Skip the apostrophe
+          } else if (next === '2') {
+            if (char === 'r') {
+              result.push('rw' as ActionKey);
+              result.push('rw' as ActionKey);
+            } else if (char === 'f') {
+              result.push('fw' as ActionKey);
+              result.push('fw' as ActionKey);
+            } else if (char === 'l') {
+              result.push('lw' as ActionKey);
+              result.push('lw' as ActionKey);
+            } else {
+              result.push(char as ActionKey);
+              result.push(char as ActionKey);
+            }
+            // Prime move
+            i++; // Skip the apostrophe*
+          } else if (char === 'r') {
+            result.push('rw' as ActionKey);
+          } else if (char === 'f') {
+            result.push('fw' as ActionKey);
+          } else if (char === 'l') {
+            result.push('lw' as ActionKey);
+          } else if (['R', 'L', 'U', 'F', 'D', 'B'].includes(char)) {
+            result.push(char as ActionKey);
+          }
+          // ignore everything else
+        }
+
+        return result;
+      });
+      console.log(sequence[index]);
+      rubis3DInstance.Recuperation_liste(sequence[index]);
+    } else {
+      console.error(
+        `Aucune instance Rubis3D trouvée pour le conteneur ${index + 1}`
+      );
+    }
   }
 
   playScene(index: number): void {
@@ -51,8 +115,20 @@ export class AlgoOLLComponent {
             } // Prime move
             i++; // Skip the apostrophe
           } else if (next === '2') {
-            result.push(char as ActionKey);
-            result.push(char as ActionKey); // Prime move
+            if (char === 'r') {
+              result.push('rw' as ActionKey);
+              result.push('rw' as ActionKey);
+            } else if (char === 'f') {
+              result.push('fw' as ActionKey);
+              result.push('fw' as ActionKey);
+            } else if (char === 'l') {
+              result.push('lw' as ActionKey);
+              result.push('lw' as ActionKey);
+            } else {
+              result.push(char as ActionKey);
+              result.push(char as ActionKey);
+            }
+            // Prime move
             i++; // Skip the apostrophe*
           } else if (char === 'r') {
             result.push('rw' as ActionKey);
